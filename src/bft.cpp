76b83e31e842d7afa2c0d50a32070686d28d1174
Tiehuis <mti@protonmail.ch>
2014-10-11 16:59:27 +1300

    Added enum, and some simple options

--- src/bft.cpp
+++ src/bft.cpp
@@ -7,18 +7,22 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <unistd.h>
 
-const std::string array = "array";
-const std::string celln = "30000";
-const std::string ptr   = "ptr";
-const std::string type  = "char";
-const size_t indentsize = 4;
- 
-void emit_line(std::vector<std::string> &output, std::string line, int indent_update = 0)
+enum class Indent { inc, dec, none };
+
+std::string array = "array";
+std::string celln = "30000";
+std::string ptr   = "ptr";
+std::string type  = "unsigned char";
+size_t indentsize = 4;
+
+void emit_line(std::vector<std::string> &output, std::string line, 
+        Indent indent_update = Indent::none)
 {
     static size_t indent = 0;
 
-    if (indent_update == -1)
+    if (indent_update == Indent::dec)
         indent -= indentsize;
 
     std::string add = "";
@@ -27,16 +31,16 @@ void emit_line(std::vector<std::string> &output, std::string line, int indent_up
     add += line;
     output.push_back(add);
 
-    if (indent_update == 1)
+    if (indent_update == Indent::inc)
         indent += indentsize;
 }
 
-std::vector<std::string>* gen_c_code(std::vector<char> &input)
+std::vector<std::string>* gen_c_code(std::vector<unsigned char> &input)
 {
-    std::vector<std::string>* out = new std::vector<std::string>();
+    auto out = new std::vector<std::string>();
 
     /* Default program start */
-    emit_line(*out, "int main(void) {", 1);
+    emit_line(*out, "int main(void) {", Indent::inc);
     emit_line(*out, type + " " + array + "[" + celln + "]" + " = {0};");
     emit_line(*out, type + " " + "*" + ptr + " = " + array + ";");
     emit_line(*out, "");
@@ -68,34 +72,59 @@ std::vector<std::string>* gen_c_code(std::vector<char> &input)
             emit_line(*out, "putchar(*" + ptr + ");");
             break;
         case ',':
-            emit_line(*out, "*" + ptr + " = getchar();");
+            emit_line(*out, "{", Indent::inc);
+            emit_line(*out, "char c = getchar();");
+            emit_line(*out, "if (c != -1) {", Indent::inc); /* Ignore EOF */
+            emit_line(*out, "*" + ptr + " = c;");
+            emit_line(*out, "}", Indent::dec);
+            emit_line(*out, "}", Indent::dec);
             break;
         case '[':
-            emit_line(*out, "while (*" + ptr + ") {", 1);
+            emit_line(*out, "while (*" + ptr + ") {", Indent::inc);
             break;
         case ']':
-            emit_line(*out, "}", -1);
+            emit_line(*out, "}", Indent::dec);
             break;
         }
     }
 
     /* Default program end */
     emit_line(*out, "return 0;");
-    emit_line(*out, "}", -1);
+    emit_line(*out, "}", Indent::dec);
     return out;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        std::cout << "Usage: bft <input.bf>" << std::endl;
+    int c;
+    int val;
+    while ((c = getopt(argc, argv, "hi:t:c:")) != -1) {
+        switch (c) {
+        case 'h':
+            std::cout << "Usage: bft <options> <input.bf>" << std::endl;
+            return 1;
+        case 'i':
+            val    = std::stoi(std::string(optarg));
+            indentsize = val <= 8 ? val : 8;
+            break;
+        case 't':
+            type = std::string(optarg);
+            break;
+        case 'c':
+            celln = std::string(optarg);
+            break;
+        }
+    }
+
+    if (optind >= argc) {
+        std::cout << "Invalid file" << std::endl;
         return 1;
     }
 
-    std::ifstream infile(argv[1]);
-    std::istream_iterator<char> start(infile), end;
-    std::vector<char> v(start, end);
-    std::vector<std::string>* output = gen_c_code(v);
+    std::ifstream infile(argv[optind]);
+    std::istream_iterator<unsigned char> start(infile), end;
+    std::vector<unsigned char> v(start, end);
+    auto output = gen_c_code(v);
 
     for (auto i : *output)
         std::cout << i << std::endl;
